92b530a190a18cd6ec19f237f1571a212b12e6fc
Michael Nnaji <mnnaji3@uwo.ca>
2019-12-24 21:24:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,7 +40,7 @@ function destructivelyAddElementToBeginningOfArray(array,element){
 function accessElementInArray(array,index){
   return(array[index])
 }
-function destructivelyRemoveElementFromEndOfArray(array,element){
+function destructivelyRemoveElementFromBeginningOfArray(array,element){
    array.shift(element)
    return(array)
 }
